58adb961d73e5fe79d921ac407a47c70cad90f24
ihappysoni <96763614+ihappysoni@users.noreply.github.com>
2022-05-21 16:03:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Form from "./components/Box"
+import Box from "./components/Box"
 function App() {
   return (
     <div className="App">
-      <Form/>
+      <Box/>
     </div>
   );
 }
